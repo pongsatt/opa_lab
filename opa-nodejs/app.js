624cd31fd2a21c7949c5015ebe03e92cdc29a6fb
Pongsatorn Tonglairoum <pongsatt@gmail.com>
2021-08-02 23:34:21 +0700

    add explanation comment

--- opa-nodejs/app.js
+++ opa-nodejs/app.js
@@ -3,16 +3,20 @@ const createOpaMiddleware = require('./opa')
 const app = express()
 const port = 3000
 
+// create opa authorization check middleware and provide api server uri
 const hasPermission = createOpaMiddleware("http://localhost:8181")
 
+// check if the request is allowed to take action "read" to object "order"
 app.get('/orders/:id', hasPermission('read', 'order'), (req, res) => {
     res.json({ message: `you can read order with id ${req.params.id}` })
 })
 
+// check if the request is allowed to take action "create" to object "order"
 app.post('/orders', hasPermission('create', 'order'), (req, res) => {
     res.json({ message: `you can create order` })
 })
 
+// start server
 app.listen(port, () => {
     console.log(`OPA NodeJs is listening at http://localhost:${port}`)
 })
--- opa-nodejs/opa.js
+++ opa-nodejs/opa.js
@@ -1,14 +1,17 @@
 const jwt = require('jsonwebtoken')
 const axios = require('axios')
 
+// create a new middleware configured with OPA api server uri
 function createOpaMiddleware(opaAgentUri) {
   const client = axios.create({
     baseURL: opaAgentUri,
   })
 
   return (action, object) => {
+    // this will be run per request
     return async (request, response, next) => {
       try {
+        // extract request's information from JWT token
         const token = request.headers.authorization
 
         if (!token) {
@@ -17,6 +20,7 @@ function createOpaMiddleware(opaAgentUri) {
 
         const decodedToken = jwt.decode(token)
 
+        // query OPA api server
         const response = await client.post(
           '/v1/data/permission/allow',
           {
@@ -28,12 +32,16 @@ function createOpaMiddleware(opaAgentUri) {
           },
         )
 
+        // OPA api server query's result
         const allow = response.data?.result
         if (!allow) {
           throw new Error("Unauthorized")
         }
+
+        // authorized
         await next()
       } catch (err) {
+        // unauthorized
         response.status(403).send(err.message)
       }
 
